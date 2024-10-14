55d16ef4dacbadbfe855a50441275019532c5f32
hannington owiti <hanningtonowiti@gmail.com>
2024-10-14 17:37:47 +0300

    models.py

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from . import db
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship('Appearance', back_populates='episode', cascade='all, delete')
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    appearances = db.relationship('Appearance', back_populates='guest', cascade='all, delete')
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+
+    episode = db.relationship('Episode', back_populates='appearances')
+    guest = db.relationship('Guest', back_populates='appearances')
